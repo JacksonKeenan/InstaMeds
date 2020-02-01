f4ead2d21c30cb47adb6af9cd923133111fdd756
JacksonKeenan <jacksonkeenan@github.com>
2020-01-31 20:10:04 -0500

    again...

--- includes/removeItem.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-  session_start();
-  unset($_SESSION['shopping_cart'][$_POST[index]]);
-  $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
--- includes/store.inc.php
+++
@@ -1,3 +0,0 @@
-<?php
-  session_start();
-  array_push($_SESSION['shopping_cart'], array('name' => $_POST[name], 'quantity' => $_POST[quantity], 'price' => $_POST[price]));
